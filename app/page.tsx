7c9f27c19b03e3e502db0ce931ad07b810b2b4b9
trixenor <trixenor@gmaim.com>
2025-11-22 18:38:37 +0000

    Mise à jour de page.tsx et package-lock.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Debut de dev</h1>;
+  return (
+    <>
+      <h1>Debut du dev</h1>
+      <p>ok yf allon avec</p>
+    </>
+  );
 }
