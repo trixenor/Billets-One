471a1cc6600fcd8ba7c13fa544f27b0f3569a623
trixenor <trixenor@gmaim.com>
2025-11-23 01:17:44 +0000

    Responsive

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,30 +1,36 @@
 import Image from "next/image";
+
 export default function Header () {
     return (
-        <header className="flex justify-between p-10">
+        <header className="flex justify-between items-center p-5">
 
-            {/* Groupe de droite */}
+            {/* Groupe gauche : Logo + Menu */}
+            <div className="flex items-center gap-5">
 
-            <div className="flflex gap-4 text-sm md:text-base text-white">
-                <div>
-                    <Image src="/logo-billets-one.png" alt="Logo" width={80} height={80} />
-                </div>
+                {/* Logo responsive */}
+                <Image
+                    src="/logo-billets-one.png"
+                    alt="Logo"
+                    width={60}
+                    height={60}
+                    className="md:w-[80px] md:h-[80px]"
+                />
 
-                <nav className="flex gap-5 text-white">
+                {/* Menu responsive */}
+                <nav className="flex gap-3 md:gap-7 text-sm md:text-base">
                     <a href="#">Accueil</a>
                     <a href="#">Contact</a>
                     <a href="#">A propos</a>
                 </nav>
             </div>
 
-             {/* Groupe de droite */}
-            
-            <div className="flex gap-4 text-sm md:text-base text-white">
-                <button className="border-white border-2 rounded-full px-4 py-2 ">
+            {/* Groupe droite : Boutons */}
+            <div className="flex items-center gap-3 md:gap-5">
+                <button className="border-white border-2 rounded-full px-3 py-1 text-sm md:px-4 md:py-2 md:text-base">
                     Se connecter
                 </button>
-                
-                <button className="border-2 rounded-full px-3 py-1 text-sm md:px-4 md:py-2 md:text-base">
+
+                <button className="bg-white text-black rounded-full px-3 py-1 text-sm md:px-4 md:py-2 md:text-base">
                     Créer un compte
                 </button>
             </div>
