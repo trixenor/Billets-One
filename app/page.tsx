4761b3786c954b024fd6294b3d6a51e32401c900
trixenor <trixenor@gmaim.com>
2025-11-23 14:39:35 +0000

    V0.2

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <>
-
-    
-    </>
+    <main className="p-15">
+      <h1 className="text-4xl font-bold mb-4">Bienvenue sur Billets-One</h1>
+      <p className="text-lg text-gray-700">
+        La plateforme pour trouver, acheter et gérer vos billets d’événements.
+      </p>
+    </main>
   );
 }
